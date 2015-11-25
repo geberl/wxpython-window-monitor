324616999cb0e6dca7bf2b8d4703820f61878d52
Günther Eberl <guenther@eberl.se>
2015-11-25 14:41:22 +0100

    Fixes for build scripts.

--- scripts/4_copy_needed_files_for_onefile_executable.bat
+++ scripts/4_copy_needed_files_for_onefile_executable.bat
@@ -1,24 +1,22 @@
 @echo off
 
 REM  Show start time of script.
-echo Started executable at %date%, %time%
+echo Started copying at %date%, %time%
 
 REM Change to parent directory of this one.
 cd "%~dp0"
 cd ..
 
-REM Change to dist/main directory.
-cd dist
-cd main
-
-REM  Run executable.
-winmon.exe
+REM Copy files.
+xcopy "icon.ico" "dist" /Y
+xcopy "logging_to_file.ini" "dist" /Y
+xcopy "settings.ini" "dist" /Y
 
 REM  Show end time of script.
-echo Executable quit at %date%, %time%
+echo Copying finished at %date%, %time%
 
 REM  Newline.
-echo.
+echo .
 
 REM  Wait for user key press to close window.
 pause
---
+++ scripts/5_run_onefile_executable.bat
@@ -0,0 +1,23 @@
+@echo off
+
+REM  Show start time of script.
+echo Started executable at %date%, %time%
+
+REM Change to parent directory of this one.
+cd "%~dp0"
+cd ..
+
+REM Change to dist directory.
+cd dist
+
+REM  Run executable.
+winmon.exe
+
+REM  Show end time of script.
+echo Executable quit at %date%, %time%
+
+REM  Newline.
+echo.
+
+REM  Wait for user key press to close window.
+pause
